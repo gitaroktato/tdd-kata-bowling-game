0477c65ae7b0619d01f04f7eb36c26d1803da68a
Oresztesz Margaritisz <oresztesz_margaritisz@epam.com>
2020-11-23 16:25:38 +0100

    :First few tests

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,14 +1,62 @@
+import bowling.Game;
 import org.junit.jupiter.api.Test;
+
+import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GameTest {
 
     @Test
-    public void testGameCreation() {
+    public void test_withOnlyOneRollWhichDoesNotHit() {
         var game = new Game();
         game.roll(0);
         var score = game.score();
-        assertEquals(0, game.score());
+        assertEquals(0, score);
     }
+
+    @Test
+    public void test_withOnlyOnePinHit() {
+        var game = new Game();
+        game.roll(1);
+        var score = game.score();
+        assertEquals(1, score);
+    }
+
+
+    @Test
+    public void test_withFullGame() {
+        var game = new Game();
+        IntStream.range(0, 20).forEach(
+                i -> game.roll(1));
+        var score = game.score();
+        assertEquals(20, score);
+    }
+
+    @Test
+    public void test_withMorePinsThanPossibleInFirstFrame() {
+        var game = new Game();
+        game.roll(10);
+        game.roll(1);
+        // ????
+    }
+
+    @Test
+    public void test_withSpare() {
+        var game = new Game();
+        game.roll(9);
+        game.roll(1);
+        game.roll(5);
+        game.roll(3);
+        assertEquals(23, game.score());
+    }
+
+    // TODO validation
+    // More than 10 pins?
+    // More than 10 frames?
+    // Getting score before all the roles?
+    // Might make sense to show the temporary results?
+
+    // TODO integrity??
+    // Two rolls with the sum of 10 in each frame.
 }
