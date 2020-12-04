91feed26531ca8b412770e6eff6b1720440c61ed
Oresztesz Margaritisz <oresztesz_margaritisz@epam.com>
2020-12-04 16:34:07 +0100

    Messed up with NoFrame

---
+++ src/main/java/bowling/NoFrame.java
@@ -0,0 +1,8 @@
+package bowling;
+
+public class NoFrame extends Frame {
+    @Override
+    int score() {
+        return 0;
+    }
+}
--- src/test/java/bowling/FrameTest.java
+++ src/test/java/bowling/FrameTest.java
@@ -41,4 +41,17 @@ public class FrameTest {
         frame.roll(10);
         assertThrows(IllegalRollException.class, () -> frame.roll(1));
     }
+
+    @Test
+    public void testRoll_withSpare() throws Exception  {
+        var frame = new Frame();
+        var nextFrame = new Frame();
+        frame.setNext(nextFrame);
+        frame.roll(3);
+        frame.roll(7);
+        nextFrame.roll(2);
+        assertEquals(12, frame.score());
+    }
+
+    // without next frame score?
 }
